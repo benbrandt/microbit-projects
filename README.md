a11c5965543d80ae46c6c3cc11aac72541e6a708
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-11-16 20:41:06 +0100

    Basic embassy and CI setup

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # micro:bit projects
 
 Experiments using Rust and embassy to write programs to run on a micro:bit v2
+
+## Get started
+
+Install [probe.rs](https://probe.rs/)
+
+```sh
+cargo binstall probe-rs-tools
+```
--- src/bin/hello_world.rs
+++ src/bin/hello_world.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+//! Prints a scrolling "Hello, world!" on a micro:bit
+#![no_std]
+#![no_main]
+#![allow(clippy::used_underscore_binding)]
+
+use embassy_executor::Spawner;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {}
