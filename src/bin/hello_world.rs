098e101e5796d41c36451508794e5f123f6e07bb
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-11-20 22:26:59 +0100

    Add scrolling hello world

--- src/bin/hello_world.rs
+++ src/bin/hello_world.rs
@@ -4,8 +4,15 @@
 #![allow(clippy::used_underscore_binding)]
 
 use embassy_executor::Spawner;
-use microbit_bsp::*;
+use microbit_bsp::{display::Brightness, Microbit};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {}
+async fn main(_spawner: Spawner) {
+    let board = Microbit::default();
+
+    let mut display = board.display;
+
+    display.set_brightness(Brightness::MAX);
+    display.scroll("Hello, World!").await;
+}
